8fa00b0ab1c1636fa0928cf8b9b89f312cc14e40
Piwer <virtual.country.standart@gmail.com>
2023-10-08 13:11:54 +0200

    Add Begin of Account System // 23v1

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import flask
 from flask import Flask, render_template, request, session, redirect, url_for, flash
+from hashlib import md5
 
 from config import config
 
@@ -9,3 +10,14 @@ app.secret_key = config.SECRET_KEY
 @app.route("/")
 def index():
     return render_template(f"index.html")
+
+@app.before_request
+def before():
+    if (not "hash" in session) or (session["hash"] == None) or (session["hash"] != md5((session["id"]+config.SECRET_KEY).encode()).hexdigest()):
+        session["id"] = "000000000"
+        session["account"] = {
+            "name": "Профиль",
+            "photo": "https://i.ibb.co/djkrZHs/profile.png",
+            "theme": "light"
+        }
+        session["hash"] = md5((session["id"]+config.SECRET_KEY).encode()).hexdigest()
