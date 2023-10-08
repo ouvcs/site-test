5aec355b45945af70fe555197642dcd78179b407
Piwer <virtual.country.standart@gmail.com>
2023-10-08 12:50:05 +0200

    Error Attribute // 23v1

--- config.py
+++ config.py
@@ -3,12 +3,10 @@ import os
 class ConfigObject():
     ISSERVER: bool = os.environ.get("secret_key") is str
     
-    SECRET_KEY: str
+    SECRET_KEY: str = "supersecretkey"
 
     def __init__(self):
         if self.ISSERVER:
             SECRET_KEY = os.environ.get("secret_key")
-        else:
-            SECRET_KEY = "supersecretkey"
             
 config = ConfigObject()
